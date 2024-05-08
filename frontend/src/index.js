2cd2fcf64d776e73e2abfad4ada7c2340f2436cc
wanshika99 <wansgikawar.99@gmail.com>
2024-05-08 22:29:55 +0530

    Added login page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -16,6 +16,7 @@ import reportWebVitals from './reportWebVitals';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
 
 
 const router = createBrowserRouter(
@@ -24,6 +25,7 @@ const router = createBrowserRouter(
           <Route index= {true} path='/' element={<HomeScreen />} />
           <Route path='/product/:id' element={<ProductScreen />} />
           <Route path='/cart' element={<CartScreen />} />
+          <Route path='/login' element={<LoginScreen />} />
     </Route>
   )
 )
